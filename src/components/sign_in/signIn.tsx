2e8a208c9610f342bde6173e7f758acd5d0998ab
William Fonseca <santifon15@gmail.com>
2023-02-23 23:12:17 -0500

    add state to sign in component

--- src/components/sign_in/signIn.tsx
+++ src/components/sign_in/signIn.tsx
@@ -1,10 +1,13 @@
+import React from "react";
 import { Link } from "react-router-dom";
 
 function SignIn() {
+   const [password, setPassword] = React.useState("");
+
    return <>
       <main className="sign-in-page">
          <h1 className="visually-hidden">Travel App</h1>
-         <form className="sign-in-form" autoComplete="off">
+         <form className="sign-in-form" autoComplete="off" onSubmit={handleSubmit}>
             <h2 className="sign-in-form__title">Sign In</h2>
             <label className="input">
                <span className="input__heading">Email</span>
@@ -12,13 +15,7 @@ function SignIn() {
             </label>
             <label className="input">
                <span className="input__heading">Password</span>
-               <input
-                  data-test-id="auth-password"
-                  name="password"
-                  type="password"
-                  autoComplete="new-password"
-                  required
-               />
+               <input data-test-id="auth-password" name="password" type="password" autoComplete="new-password" value={password} onChange={(e) => setPassword(e.target.value)} required />
             </label>
             <button data-test-id="auth-submit" className="button" type="submit">
                Sign In
@@ -26,16 +23,21 @@ function SignIn() {
          </form>
          <span>
             Don't have an account?
-            <Link
-               data-test-id="auth-sign-up-link"
-               href="./sign-up.html"
-               className="sign-in-form__link"
-            >
+            <Link data-test-id="auth-sign-up-link" to={"/sign-up"} className="sign-in-form__link">
                Sign Up
             </Link>
          </span>
       </main>
    </>
+
+   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+      e.preventDefault();
+      if (!(password.length >= 3 && password.length <= 20)) {
+         alert("Password must be between 3 and 20 characters")
+         return;
+      }
+      window.location.href = "/";
+   }
 }
 
 export default SignIn;
