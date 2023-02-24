6d32a32e5e9a1ee4a7ad322783e98a93fde0a5b5
William Fonseca <santifon15@gmail.com>
2023-02-23 23:04:08 -0500

    update sign-up component

--- src/components/sign_up/signUp.tsx
+++ src/components/sign_up/signUp.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { PassThrough } from "stream";
 
 
 function SingUp() {
    const [password, setPassword] = React.useState("");
-   const [btnSubmit, setBtnSubmit] = React.useState(false);
 
    return (
       <main className="sign-up-page">
